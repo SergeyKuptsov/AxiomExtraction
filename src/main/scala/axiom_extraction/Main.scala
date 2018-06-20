624655652c5a231f7be5b12abd19038def884553
SergeyKuptsov <sergeykuptsovde@gmail.com>
2018-06-20 22:12:47 +0200

    Transactiongeneration

--- src/main/scala/axiom_extraction/Main.scala
+++ src/main/scala/axiom_extraction/Main.scala
@@ -6,7 +6,8 @@ import org.apache.spark.sql.SparkSession
 import org.apache.jena.riot.Lang
 import org.apache.spark.sql.{DataFrame, SQLContext}
 import org.apache.spark.{SparkConf, SparkContext}
-
+import scala.collection.mutable.ListBuffer
+import org.apache.spark.sql.functions._
 object Main extends App {
 
   var DEBUG: Boolean = false
@@ -17,21 +18,36 @@ object Main extends App {
       .master("local[*]")
       .config("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
       .getOrCreate()
-     val resource = s"test_input.nt"
-     val path = getClass.getResource(resource).getPath()
-     val df: DataFrame = spark.read.rdf(Lang.NTRIPLES)(path)
-       df.show
-    val selectedData = df.select("s", "p", "o").where("p == 'friendOf' OR p == 'hates'")
+    val resource = "test_input.nt"
+    val path = getClass.getResource(resource).getPath()
+    val df: DataFrame = spark.read.rdf(Lang.NTRIPLES)(path)
+    df.show()
+    
+    //selecting all distinct predicates
     val distinctValuesDF = df.select("p").distinct
+    //selecting all distinct instances
     val distinctSubjectsDF = df.select("s").distinct
     val distinctObjectsDF = df.select("o").distinct
-    selectedData.show()
     distinctValuesDF.show()
-    val instances = distinctSubjectsDF.union(distinctObjectsDF).distinct()
-    distinctValuesDF.select("p").take((distinctValuesDF.count()).toInt).foreach(printWithNeg)
     
-    instances.show()
-
+    //all distinct instances in a list
+    val instances = distinctSubjectsDF.union(distinctObjectsDF).distinct().rdd.map(r => r(0)).collect()
+    
+    //list of subsamples for instance pair extraction
+    var dfList : ListBuffer[DataFrame] = new ListBuffer()
+    
+    var transactions : ListBuffer [String] = new ListBuffer()
+    
+    //printing out distinct predicate values
+    distinctValuesDF.select("p").take((distinctValuesDF.count()).toInt).foreach(printWithNeg)    
+    
+    //sampling the pair groups
+    instances.foreach {x => getInstanceFilter(x,df,dfList)}
+    
+    
+    //extracting pairs and generating transactions
+    dfList.foreach(x=>extractTransactions(x,transactions))
+    transactions.foreach(println)
   }
 
 	def debug(out: String) {
@@ -43,9 +59,42 @@ object Main extends App {
 	  Console.println(x)	  
 	  Console.println("[not_"+x.toString().takeRight(x.toString().length()-1))
 	}
+	
+	def extractTransactions(df :DataFrame,transactions :ListBuffer[String]) {
+	  var count:Int = 0
+	  var transactions : ListBuffer[String] = new ListBuffer[String]
+    var outString: String = ""
+    var currObject: String = ""                        
+    count = df.count().toInt         
+    currObject = df.sort("o").first()(2).toString()
+    outString = "(" + df.sort("o").first()(0).toString() + ":" + df.sort("o").first()(2).toString() + ") -> "
+    df.sort("o").foreach{ x=>
+              	    { Console.println(x.toString())
+            	                                    	  
+              	      outString += x(1) +","
+              	      outString += "not_"+x(1).toString() +"," 
+              	      Console.println(outString)              	    }  
+              	   
+      }
+	   transactions.append(outString.take(outString.length()-1))
+     Console.println("TR Size: " + transactions.size.toString())       	   
+     Console.println("===================================================")              
+	  }
+	
+	
+	def getInstanceFilter(x:Any,df:DataFrame,lDf:ListBuffer[DataFrame]) {
+	  val output=df.select("s","p","o").where("s == '"+x.toString()+"' ")
+	  val instances = output.select("o").distinct().rdd.map(r => r(0)).collect()
+	  instances.foreach(f =>{
+	    val subOutput=output.select("s","p","o").where("o == '"+f.toString()+"' ")
+  	  if(subOutput.count()>0)//if at least one paire exists
+  	  {
+  	    lDf.append(subOutput.sort(asc("o")))
+  	  }
+	  })
+
+	}
 }
-
-
 
 
 
